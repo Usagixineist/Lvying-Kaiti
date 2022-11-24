d781e7aad4a4fb574f5c88655726fedfad6dc710
Usagixineist <1947405077@qq.com>
2022-11-24 17:46:43 +0800

    base

--- change.py
+++ change.py
@@ -4,7 +4,7 @@ import re
 with open('characters base.md', encoding='utf-8', mode='r+') as base:
     doc = base.read()
     doc1 = re.sub(r'[*][*]([^*]+)[*][*]',
-                  r'<span style="color:green">\1</span>', doc)
+                  r'<span style="color:red">\1</span>', doc)
     doc2 = re.sub(r'[~][~]([^~]+)[~][~]',
                   r'<span style="color:white">\1</span>', doc1)
     with open('characters.md', encoding='utf-8', mode='r+') as f:
